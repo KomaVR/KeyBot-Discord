192f6cf5b4c98d4307ae88426b6107b39684f011
KomaVR <komavr12454@gmail.com>
2025-06-23 14:03:43 -0700

    Update key_system_bot.py

--- key_system_bot.py
+++ key_system_bot.py
@@ -18,7 +18,7 @@ TOKEN           = os.getenv('DISCORD_TOKEN')
 ADMIN_ROLE_NAME = os.getenv('ADMIN_ROLE_NAME', 'KeyManager')
 OWNER_ID        = int(os.getenv('OWNER_ID', '0'))
 HMAC_SECRET     = os.getenv('HMAC_SECRET', '')
-GIST_TOKEN      = os.getenv('KEYS_GIST_TOKEN')
+KEYS_GIST_TOKEN = os.getenv('KEYS_GIST_TOKEN')
 GIST_ID         = os.getenv('GIST_ID')
 
 # Load or init panel config
